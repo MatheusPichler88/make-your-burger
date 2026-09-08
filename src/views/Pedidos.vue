5c09e6bdb93824dd7fbb9e418b00ac04ac2037b7
MatheusPichler88 <matheuspichler94@gmail.com>
2026-09-08 18:32:14 -0300

    feat: criando tela de pedidos

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,12 +1,18 @@
 <template>
   <!-- Usa o mesmo container global para manter o alinhamento visual -->
   <div class="main-container">
-    <h1>Pedidos</h1>
+    <h1>Gerenciar pedidos:</h1>
+    <Dashboard />
   </div>
 </template>
 
 <script>
+import Dashboard from '@/components/Dashboard.vue';
+
 export default {
-  name: 'Pedidos' // Identificador da View de pedidos
+  name: 'Pedidos', // Identificador da View de pedidos
+  components: {
+    Dashboard
+  }
 }
 </script>
